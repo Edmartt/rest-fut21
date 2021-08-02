105217e4b6590d723a0ef55fdabe5808f8996847
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-08-02 17:10:39 -0500

    Added endpoint security with x-api-key

--- api/main/views.py
+++ api/main/views.py
@@ -1,30 +1,48 @@
-from flask import make_response, jsonify, abort, request
+from flask import make_response, jsonify, abort, request, current_app
 from . import main
 from ..team_players import Team
 
 
 @main.errorhandler(404)
 def not_found(error):
+    '''Personaliza la respuesta a JSON en caso de error.'''
     return make_response(jsonify({'Error': 'Not Found'}), 404)
 
 
 @main.route('/api/v1/team/<string:name>', methods=['GET'])
 def get_team_players(name):
+    '''Obtiene los jugadores de un equipo.'''
+
+    headers = request.headers
+    auth = headers.get('X-Api-Key')  # Capturamos la api key que vaya en el header enviada por el cliente
+    api_key = current_app.config['API_KEY']  # asignamos lo que tengamos en la variable de entorno como api_key
     team = Team()
 
-    players = team.get_players(name)
-    if players is None:
-        abort(404)
-    return make_response(jsonify(players), 200)
+    if auth == api_key:
+        players = team.get_players(name)
+        if players is None:
+            abort(404)    
+        return make_response(jsonify(players), 200)
+        
+    return make_response(jsonify({'message': 'Not Authorized'}), 401)
 
 
 @main.route('/api/v1/players')
 def get_data():
+    '''Busca jugadores con coincidencias parciales o totales.
 
+    Acepta querystrings: search="nombre de jugador"&order.
+    El orden puede ser asc o desc, por defecto es asc, incluso si se omite.
+    '''
     args = request.args
     team = Team()
     result = team.get_player_string(args)
+    headers = request.headers
+    auth = headers.get('X-Api-Key')  # Capturamos la api key que vaya en el header enviada por el cliente
+    api_key = current_app.config['API_KEY']  # asignamos lo que tengamos en la variable de entorno como api_key
 
-    if result is None:
-        abort(404)
-    return make_response(jsonify(result), 200)
+    if auth == api_key:
+        if result is None:
+            abort(404)
+        return make_response(jsonify(result), 200)
+    return make_response(jsonify({'message': 'Not Authorized'}), 401)
