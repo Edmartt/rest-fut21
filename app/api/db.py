e6ce3bac8b56cfd53f5c099d3191773631dce128
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-09-12 01:40:08 -0500

    Now get_db, init_db and close db are methods of a class called Database manager for better cohesion

--- app/api/db.py
+++ app/api/db.py
@@ -5,41 +5,42 @@ import mysql.connector
 from .schema import instructions
 
 
-def get_db():
-    if 'db' not in g:
-        g.db = mysql.connector.connect(
-            host=current_app.config['MYSQL_HOST'],
-            user=current_app.config['MYSQL_USER'],
-            password=current_app.config['MYSQL_PASSWORD'],
-            database=current_app.config['MYSQL_DATABASE']
-                )
-        g.c = g.db.cursor(dictionary=True)
+class DatabaseManager:
+    def get_db(self):
+        if 'db' not in g:
+            g.db = mysql.connector.connect(
+                    host=current_app.config['MYSQL_HOST'],
+                    user=current_app.config['MYSQL_USER'],
+                    password=current_app.config['MYSQL_PASSWORD'],
+                    database=current_app.config['MYSQL_DATABASE']
+                    )
+            g.c = g.db.cursor(dictionary=True)
 
-        return g.db, g.c
+            return g.db, g.c
+
+    def close_db(self, e=None):
+        db_connect = g.pop('db', None)
+
+        if db_connect is not None:
+            db_connect.close()
+
+    def init_db(self):
+        db_connect, db_cursor = self.get_db()
+
+        for i in instructions:
+            db_cursor.execute(i)
+        db_connect.commit()
 
 
-def close_db(e=None):
-    db_connect = g.pop('db', None)
-
-    if db_connect is not None:
-        db_connect.close()
-
-
-def init_db():
-    db_connect, db_cursor = get_db()
-
-    for i in instructions:
-        db_cursor.execute(i)
-    db_connect.commit()
+connector = DatabaseManager()
 
 
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
-    init_db()
-#    click.echo('Database tables created')
+    connector.init_db()
 
 
 def init_app(app):
-    app.teardown_appcontext(close_db)
+    app.teardown_appcontext(connector.close_db)
     app.cli.add_command(init_db_command)
