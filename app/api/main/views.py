dc7d3a69c3f865880db1235b7092fc8cbcc7dd75
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-09-12 23:35:35 -0500

    Added error handlers for the different http error responses. Created a decorator function for asking a api key authentication and added the Class Coincidences for the representation of the players search endpoint

--- app/api/main/views.py
+++ app/api/main/views.py
@@ -9,31 +9,77 @@ from ..team_players import Team
 
 @main.errorhandler(404)
 def not_found(error):
-    '''Personaliza la respuesta a JSON en caso de error.'''
+    '''Custom answer in json format for 404 error'''
     return make_response(jsonify({'Error': 'Not Found'}), 404)
 
 
+@main.errorhandler(403)
+def forbidden(error):
+    '''Custom answer in json format for 403 error'''
+    return make_response(jsonify({'Error': 'Forbidden'}), 403)
+
+
+@main.errorhandler(401)
+def not_authorized(error):
+    '''Custom answer in json format for 401 error'''
+    return make_response(jsonify({'Error': 'Not Authorized'}), 401)
+
+#  ------------------------------------------------------------------------------
+
+
+def auth_required(func):
+    '''Handles the authentication with header x-api-key.
+
+    Checks if the api_key sent in the header is the same that the one
+    in the environment variable.
+    '''
+    def decorator(*args, **kwargs):
+        auth = request.headers.get('X-API-KEY')
+        if auth != current_app.config['API_KEY']:
+            abort(401)
+        return func(*args, **kwargs)
+    return decorator
+
+
 class TeamPlayers(MethodView):
+    '''Class representing show_players view function.'''
+    decorators = [auth_required]
 
     def __init__(self, team: Team, querygen: QueryGenerator):
         self.team = team
         self.querygen = querygen
 
     def get(self, team_name: str):
-        headers = request.headers
-        auth = headers.get('X-Api-Key')  # Capturamos api key enviada por el cliente
-        api_key = current_app.config['API_KEY']  # asignamos la variable de entorno como api_key
+        players = self.team.get_players(team_name, self.querygen)
+        if players is None:
+            abort(404)
+        return make_response(jsonify(players), 200)
 
-        if auth == api_key:
-            players = self.team.get_players(team_name, self.querygen)
 
-            if players is None:
-                abort(404)
-            return make_response(jsonify(players), 200)
-        return make_response(jsonify({'message': 'Not Authorized'}), 401)
+class Coincidences(MethodView):
+    '''Class representing get_player view function'''
+    decorators = [auth_required]
 
+    def __init__(self, team: Team, querygen: QueryGenerator):
+        self.team = team
+        self.querygen = querygen
+
+    def get(self):
+        args = request.args
+        result = self.team.get_player_string(args, self.querygen)
+        if result is None:
+            abort(404)
+        return make_response(jsonify(result), 200)
+
+
+teams_view = TeamPlayers.as_view('show_players', Team(),
+                                 QueryGenerator(DatabaseManager()))
+
+players_view = Coincidences.as_view('get_player', Team(),
+                                    QueryGenerator(DatabaseManager()))
 
 main.add_url_rule('/api/v1/teams/<string:team_name>',
-                  view_func=TeamPlayers.as_view
-                  ('get_players', Team(), QueryGenerator(DatabaseManager())),
+                  defaults={'team_name': None}, view_func=teams_view,
                   methods=['GET'])
+
+main.add_url_rule('/api/v1/players', view_func=players_view, methods=['GET'])
