1f26c555ae835f373147153a9be6259cb23bd25d
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-09-18 12:55:05 -0500

    Added internal server error

--- app/api/main/errors.py
+++ app/api/main/errors.py
@@ -23,3 +23,8 @@ def not_authorized(error):
 @main.errorhandler(400)
 def bad_request(error):
     return make_response(jsonify({'Error': 'Bad Request'}), 400)
+
+
+@main.errorhandler(500)
+def internal_server_error(error):
+    return make_response(jsonify({'Error': 'Internal Server Error'}))
